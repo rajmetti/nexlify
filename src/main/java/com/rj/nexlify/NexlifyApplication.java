749a6ba4b68f0b716d695b654633517466d0fdea
raj Metti <rajmetti@gmail.com>
2025-05-06 16:58:43 +0530

    initial commit

---
+++ src/main/java/com/rj/nexlify/NexlifyApplication.java
@@ -0,0 +1,13 @@
+package com.rj.nexlify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexlifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexlifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rj/nexlify/NexlifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rj.nexlify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexlifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
